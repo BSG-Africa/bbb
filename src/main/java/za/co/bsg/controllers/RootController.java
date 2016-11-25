4a6784980b6c40b6275c4ae845af7ec20840c63c
Ziyad.Jappie <ziyad.jappie@bsg.co.za>
2016-11-25 16:01:51 +0200

    Run app using tomcat to host

--- src/main/java/za/co/bsg/controllers/RootController.java
+++ src/main/java/za/co/bsg/controllers/RootController.java
@@ -2,6 +2,7 @@ package za.co.bsg.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import za.co.bsg.dataAccess.UserRoleDetailsDAO;
 import za.co.bsg.models.UserRole;
 
@@ -14,7 +15,16 @@ public class RootController {
         this.userRoleDetailsDAO = userRoleDetailsDAO;
     }
 
-    public void PersistStuff() {
+    @RequestMapping("/")
+    public String home() {
+        System.out.print("Hello");
+//        this.PersistStuff();
+
+        // fix database interaction
+        return "index";
+    }
+
+    private void PersistStuff() {
         UserRole userRole = new UserRole("Moderator");
         userRoleDetailsDAO.save(userRole);
     }
