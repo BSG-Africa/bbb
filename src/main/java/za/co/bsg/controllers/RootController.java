c37d29bfd473a4458632eb7c7c3b64a7bd218604
Ziyad.Jappie <ziyad.jappie@bsg.co.za>
2016-11-29 11:00:29 +0200

    persist userole object to user table

--- src/main/java/za/co/bsg/controllers/RootController.java
+++ src/main/java/za/co/bsg/controllers/RootController.java
@@ -2,8 +2,10 @@ package za.co.bsg.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import za.co.bsg.dataAccess.UserRoleDetailsDAO;
+import za.co.bsg.models.User;
 import za.co.bsg.models.UserRole;
 
 @Controller
@@ -18,15 +20,38 @@ public class RootController {
     @RequestMapping("/")
     public String home() {
         System.out.print("Hello");
-//        this.PersistStuff();
-
-        // fix database interaction
+//        this.PersistStuff(); // to test persist
         return "index";
     }
 
+//    @RequestMapping(value = "/getUserDetails/", method = RequestMethod.POST)
+//    @ResponseBody
+//    public void postGetUsers(@RequestParam String userCred, @RequestParam String pass){
+//        String name = userCred;
+//        String passs = pass;
+////        String passwordForUSer = password;
+//
+//    }
+
+    @RequestMapping(value = "getUserDetails", method = RequestMethod.POST)
+    public String validatePageSubmit(@ModelAttribute(value = "user") User user, Model model) {
+        String username = user.getUserName();
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/getUserDetail/{getUser}", method = RequestMethod.GET)
+    @ResponseBody
+    public void getUsers(@PathVariable String object) {
+        String name = object;
+//        String passwordForUSer = password;
+
+    }
     private void PersistStuff() {
         UserRole userRole = new UserRole("Moderator");
+        User user = new User("kapeshi.kongolo", "Welcome", "Kapeshi", "Kongolo", userRole);
         userRoleDetailsDAO.save(userRole);
+        userRoleDetailsDAO.saveUser(user);
     }
 
 }
