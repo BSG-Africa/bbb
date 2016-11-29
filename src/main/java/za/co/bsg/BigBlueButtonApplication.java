fddb459bf344e8294c3b018ced82124714b3f6ae
Ivhani Maselesele <Ivhani.Maselesele@bsg.co.za>
2016-11-29 13:58:44 +0200

    Remove tomcat version

--- src/main/java/za/co/bsg/BigBlueButtonApplication.java
+++ src/main/java/za/co/bsg/BigBlueButtonApplication.java
@@ -3,6 +3,8 @@ package za.co.bsg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootConfiguration
-public class BigBlueButtonApplication {
+public class BigBlueButtonApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BigBlueButtonApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<BigBlueButtonApplication> applicationClass = BigBlueButtonApplication.class;
 }
