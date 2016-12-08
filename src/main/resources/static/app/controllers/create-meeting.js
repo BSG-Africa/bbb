0af88033fafaec49df17d894be00cd45ef1feb5f
Ivhani Maselesele <Ivhani.Maselesele@bsg.co.za>
2016-12-08 11:14:40 +0200

    Get myMeetings from meeting management service

--- src/main/resources/static/app/controllers/create-meeting.js
+++ src/main/resources/static/app/controllers/create-meeting.js
@@ -4,7 +4,7 @@ angular.module('BigBlueButton')
             $scope.user = AuthService.user;
             // TODO : Ivhani Please remove this
             $scope.meeting.createdBy = $scope.user.principal.name;
-            $scope.meeting.status = "Active";
+            $scope.meeting.status = "Not started";
             $http.post('/api/meeting/create', $scope.meeting).success(function (res) {
 
                 $scope.message = "Meeting creation successfull !";
