1591832ddb1f426e360513ee093175c2f59fa23d
Ivhani Maselesele <Ivhani.Maselesele@bsg.co.za>
2016-12-09 10:20:44 +0200

    Add more fields to front end meeting creation page

--- src/main/resources/static/app/controllers/create-meeting.js
+++ src/main/resources/static/app/controllers/create-meeting.js
@@ -1,8 +1,15 @@
 angular.module('BigBlueButton')
     .controller('CreateMeetingController', function ($http, $scope, AuthService, $state) {
         $scope.createMeeting = function () {
+            $scope.additionalInfo = '1';
+            for (var i = 0; i < $scope.additionalInformationElemnt.length; i++) {
+                $scope.additionalInfo = $scope.additionalInfo + $scope.additionalInformationElemnt[i].additionalInformation + ': ' + $scope.additionalInformationElemnt[i].answer + ',';
+            }
+            console.log($scope.additionalInfo);
+            //$scope.meeting.agenda = $scope.additionalInfo;
             $scope.user = AuthService.user;
             // TODO : Ivhani Please remove this
+            $scope.meeting.agenda = $scope.additionalInfo;
             $scope.meeting.createdBy = $scope.user.principal.id;
             $scope.meeting.status = "Not started";
             $http.post('/api/meeting/create', $scope.meeting).success(function (res) {
@@ -13,5 +20,47 @@ angular.module('BigBlueButton')
                 $scope.message = error.message;
             });
         };
+
+
+        var counter = 0;
+        $scope.additionalInformationElemnt = [{
+            id: counter,
+            additionalInformation: 'Item name (click to change)',
+            answer: '',
+            inline: true
+        }];
+
+
+        $scope.newItem = function ($event) {
+
+            /*            $scope.additionalInfo = '1';
+             for (var i = 0; i < $scope.additionalInformationElemnt.length; i++) {
+             $scope.additionalInfo = $scope.additionalInfo + $scope.additionalInformationElemnt[i].additionalInformation + ': ' +$scope.additionalInformationElemnt[i].answer + ',';
+             }
+             console.log($scope.additionalInfo);*/
+
+
+            counter++;
+            $scope.additionalInformationElemnt.push({
+                id: counter,
+                additionalInformation: 'Item name (click to change)',
+                answer: '',
+                inline: true
+            });
+            $event.preventDefault();
+        }
+        /*        $scope.inlinef= function($event,inlinecontrol){
+         var checkbox = $event.target;
+         if(checkbox.checked){
+         $('#'+ inlinecontrol).css('display','inline');
+         }else{
+         $('#'+ inlinecontrol).css('display','');
+         }
+
+         }
+         $scope.showitems = function($event){
+         $('#displayitems').css('visibility','none');
+         }*/
+
 
     });
