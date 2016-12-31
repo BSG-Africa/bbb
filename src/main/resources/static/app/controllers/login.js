5d36ad8a4dcafb5102123e3104014d726df7cec7
Kapeshi Flavier Kongolo <kapeshikongolo@gmail.com>
2017-01-01 00:23:54 +0200

    add login service to allow auto login from register

--- src/main/resources/static/app/controllers/login.js
+++ src/main/resources/static/app/controllers/login.js
@@ -1,35 +1,9 @@
 angular.module('BigBlueButton')
 // Creating the Angular Controller
-    .controller('LoginController', function ($http, $scope, $state, AuthService, $rootScope, $sessionStorage) {
+    .controller('LoginController', function ($http, $scope, $state, AuthService, LoginService) {
 
         // method for login
         $scope.login = function () {
-            // creating base64 encoded String from user name and password
-            var base64Credential = btoa($scope.username + ':' + $scope.password);
-
-            // calling GET request for getting the user details
-            $http.get('user', {
-                headers: {
-                    // setting the Authorization Header
-                    'Authorization': 'Basic ' + base64Credential
-                }
-            }).success(function (res) {
-                $scope.password = null;
-                if (res.authenticated) {
-                    $scope.message = '';
-                    // setting the same header value for all request calling from
-                    // this application and adding user to session storage for refresh
-                    $http.defaults.headers.common['Authorization'] = 'Basic ' + base64Credential;
-                    AuthService.user = res;
-                    $sessionStorage.loggedUser = res;
-                    $sessionStorage.loggedAuth = 'Basic ' + base64Credential;
-                    $rootScope.$broadcast('LoginSuccessful');
-                    $state.go('meeting');
-                } else {
-                    $scope.message = 'Authentication Failed !';
-                }
-            }).error(function (error) {
-                $scope.message = 'Authentication Failed !';
-            });
+            LoginService.loginUser($scope.username, $scope.password, $scope);
         };
     });
--- src/main/resources/static/app/controllers/register.js
+++ src/main/resources/static/app/controllers/register.js
@@ -1,15 +1,16 @@
 angular.module('BigBlueButton')
 // Creating the Angular Controller
-    .controller('RegisterController', function ($http, $scope, $state, $rootScope) {
+    .controller('RegisterController', function ($http, $scope, $state, $rootScope, LoginService) {
         if($state.current.name == 'register'){
             $rootScope.$broadcast('NavigatedOnRegister');
         }
         $scope.submit = function () {
             $http.post('register', $scope.appUser).success(function (res) {
-                $scope.appUser = null;
-                $scope.confirmPassword = null;
                 $scope.register.$setPristine();
                 $scope.message = "Registration successful !";
+                LoginService.loginUser($scope.appUser.username, $scope.appUser.password, $scope);
+                $scope.appUser = null;
+                $scope.confirmPassword = null;
             }).error(function (error) {
                 $scope.message = error.message;
             });
