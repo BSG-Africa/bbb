bb4879624896aa34e27d450a68f9fbf0846e90cd
Kapeshi Flavier Kongolo <kapeshikongolo@gmail.com>
2017-01-09 23:16:15 +0200

    Remove create/edit meeting page and add all functionality on meeting page

--- src/main/resources/static/app/controllers/meeting.js
+++ src/main/resources/static/app/controllers/meeting.js
@@ -1,15 +1,29 @@
 angular.module('BigBlueButton')
-    .controller('MeetingController', function ($http, $scope, AuthService, $state, $stateParams, $window, $rootScope, $timeout, $location) {
+    .controller('MeetingController', function ($http, $scope, AuthService, $state, $stateParams, $window, $rootScope, $timeout, Upload, $modal) {
+        var edit = false;
         $scope.user = AuthService.user;
         $scope.name = $scope.user.principal.name;
         $scope.meetingName = $stateParams.meetingName;
+        $scope.buttonText = 'Create';
+
+        // App variable to show the uploaded response
+        $scope.responseData = undefined;
+
+        $scope.$watch('file', function () {
+            if ($scope.file != null) {
+                $scope.upload($scope.file);
+            }
+        });
 
         $scope.rowHighlighted = function (row) {
             $scope.myMeetingsSelectedRow = row;
         };
 
-        $scope.createMeeting = function () {
-            $state.go('create-meeting');
+        $scope.addMeeting = function () {
+            edit = false;
+            $scope.myMeetingsSelectedRow = undefined;
+            $scope.meeting = $scope.myMeeting[$scope.myMeetingsSelectedRow];
+            $scope.buttonText = 'Create';
         };
 
         $scope.getUsersBySearchTerm = function (searchTerm) {
@@ -28,18 +42,55 @@ angular.module('BigBlueButton')
             return null;
         };
 
-
         $scope.editMeeting = function () {
-            if ($scope.myMeetingsSelectedRow === undefined) {
-                $scope.message = 'Please select a row';
-                $timeout(function () {
-                    $scope.message = undefined;
-                }, 4000);
+            edit = true;
+            $scope.message = '';
+            $scope.meeting = $scope.myMeeting[$scope.myMeetingsSelectedRow];
+            $scope.buttonText = 'Update';
+        };
+
+        $scope.tooltip = {
+            "title": "Click below to Upload the file or Drag and Drop the file below",
+            "checked": false
+        };
+
+        // upload on file select or drop
+        $scope.upload = function (file) {
+            $scope.responseData = '';
+            Upload.upload({
+                url: 'upload',
+                file: file
+            }).then(function (resp) {
+                $scope.meeting.defaultPresentationURL = resp.data.url;
+                $scope.responseData = resp.data.response;
+                console.log($scope.meeting);
+            }, function (resp) {
+                $scope.responseData = 'Error: Failed to upload the file';
+                console.log(resp);
+            }, function (evt) {
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                $scope.responseData = 'Progress: ' + progressPercentage + '%';
+            });
+        };
+
+        $scope.createMeeting = function () {
+            if(!(typeof $scope.meeting.moderator == "object")){
+                $scope.message = 'Please select a valid Moderator Name';
+            } else if ($scope.meeting.id > 0) {
+                $http.post('api/meeting/edit', $scope.meeting).success(function (res) {
+                    $scope.closeModal();
+                }).error(function (error) {
+                    $scope.message = error.message;
+                });
             } else {
-                $state.go('edit-meeting/:id', {
-                    id: $scope.myMeeting[$scope.myMeetingsSelectedRow].id,
-                    allUsers: $scope.allUsers,
-                    availableMeetings: $scope.myMeeting[$scope.myMeetingsSelectedRow]
+                $scope.user = AuthService.user;
+                $scope.meeting.createdBy = $scope.user.principal;
+                $scope.meeting.status = "Not started";
+                $http.post('api/meeting/create', $scope.meeting).success(function (res) {
+                    getMyMeetings ();
+                    $scope.closeModal();
+                }).error(function (error) {
+                    $scope.message = error.message;
                 });
             }
         };
