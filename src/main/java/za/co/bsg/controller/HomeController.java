badcfed717ffbea51eec55deec0aeb1131db747f
tiyani <tiyani.rikhotso@bsg.co.za>
2017-01-12 14:28:33 +0200

    Add comments to controllers and services

--- src/main/java/za/co/bsg/controller/HomeController.java
+++ src/main/java/za/co/bsg/controller/HomeController.java
@@ -26,6 +26,16 @@ public class HomeController {
     @Autowired
     MeetingManagementService meetingManagementService;
 
+    /**
+     * This method creates a user object in the user table
+     * if user does not exist in the user table a RunTimeException is thrown
+     * if user exists, a user role, blocked status and hashed password are set
+     * then the user object is persisted in the user table.
+     *
+     * @param appUser a User data type - User object containing details for creating a
+     *                user object in the user table
+     * @return a ResponseEntity<User> containing persisted user object and a created HttpStatus
+     */
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<User> createUser(@RequestBody User appUser) {
         if (userRepository.findUserByUsername(appUser.getUsername()) != null) {
@@ -38,6 +48,18 @@ public class HomeController {
         return new ResponseEntity<User>(userRepository.save(appUser), HttpStatus.CREATED);
     }
 
+    /**
+     * This method generates a MeetingInvite that is used to join a meeting by
+     * getting the invite url using bbb meeting fullName and meetingId
+     * and meeting by meetingId, then using the the inviteUrl and meeting
+     * to construct a MeetingInvite object
+     *
+     * @param fullName a String data type - a fullName of the bbb meeting
+     *                 used to retrieve invite url
+     * @param meetingId a String data type - a meetingId of a bbb meeting
+     *                  used to retrieve a meeting
+     * @return a MeetingInvite object
+     */
     @RequestMapping(value = "/invite", method = RequestMethod.GET)
     public @ResponseBody MeetingInvite joinInvite(@RequestParam String fullName, @RequestParam String meetingId) {
         String url = meetingManagementService.getInviteURL(fullName, meetingId);
@@ -51,6 +73,12 @@ public class HomeController {
         return invite;
     }
 
+    /**
+     * This method returns a principal user
+     *
+     * @param principal a Principal data type - a principal user to be returned
+     * @return a Principal object
+     */
     @RequestMapping("/user")
     public Principal user(Principal principal) {
         return principal;
